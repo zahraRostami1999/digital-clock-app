4077e4fa7f52c80b648779f217258e584ec762ac
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-23 12:18:37 +0330

    edit App.js: add Clock and Stopwatch Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Clock from './Clock.jsx';
-import StopWatch from './StopWatch.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Clock from './Clock';
+import StopWatch from './StopWatch';
 
 function App() {
+  document.title = 'Clock';
   return (
-    <div className="App">
-      <Clock />
-      <StopWatch />
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Clock />} />
+            <Route path="/stopWatch" element={<StopWatch />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+
   );
 }
 
