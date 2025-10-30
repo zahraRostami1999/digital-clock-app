052469d1ff5867880f7b739bba3183aa651ba79f
Zahra Rostami <zahrarostami137878@gmail.com>
2025-10-30 19:27:14 +0330

    04-08-08-Fix ui

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,39 +1,37 @@
 import styles from './Clock.module.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function Clock(){
-    const [time, setTime] = useState(new Date());
-    
-    useEffect(() => {
-        const timerID = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
+function Clock() {
+  const [time, setTime] = useState(new Date());
 
-        return () => {
-            clearInterval(timerID);
-        }
-    }, [])
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timerID);
+  }, []);
 
-    function formatTime() {
-        const hour = time.getHours();
-        const minute = time.getMinutes();
-        const second = time.getSeconds();
+  const formatTime = () => {
+    const h = String(time.getHours()).padStart(2, '0');
+    const m = String(time.getMinutes()).padStart(2, '0');
+    const s = String(time.getSeconds()).padStart(2, '0');
+    return { h, m, s };
+  };
 
-        return (`${padZero(hour)}:${padZero(minute)}:${padZero(second)}`);
-    }
+  const { h, m, s } = formatTime();
 
-    function padZero(value) {
-        return value < 10 ? '0' + value : value;
-    }
-
-    return(
-        <>
-        <div className={styles.clockContainer}>
-            <span>{formatTime()}</span>
-            <button><a href="/stopWatch">Stop Watch</a></button>
-        </div>
-        </>
-    );
+  return (
+    <div className={styles.clockContainer}>
+      <div className={styles.digitalTime}>
+        {h}:{m}:<span className={styles.second}>{s}</span>
+      </div>
+      
+      <Link to="/stopWatch" className={styles.navButton}>
+        Stop Watch
+      </Link>
+    </div>
+  );
 }
 
 export default Clock;
